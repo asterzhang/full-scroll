47b561aad8b157dbdd7bb46c6b5788312d883f23
yuqin <yuqinweb@qq.com>
2014-11-11 23:36:17 +0800

    update

--- README.md
+++ README.md
@@ -1,17 +1,39 @@
-# full-scroll
+# full scroll
 
-A full page scroll jQuery plugin.
+full scroll，又一个快速实现整屏滚动的jQuery插件，兼容所有浏览器。
 
-live demo:http://yuqin.github.io/full-scroll/demo.html
+live demo:http://yuqin.github.io/full-scroll/index.html
 
-## How to use
 
-1. todo 
-2. todo ...Download jquery.fullscroll.js/jquery.fullscroll.min.js
+## 为什么要写这个插件
+
+这两年我写了好几个整屏滚动的页面，是时候沉淀下，将整屏滚动插件化，。
+
+直到前两周才发现，已经有很多这样的jQuery插件了，比如fullpage、onepage-scroll。
+
+fullpage：太重量级了，一个插件将近2000行代码，想想也是醉了。
+
+onepage-scroll：基本没人在维护了，bug比较多，优化的空间也很多。
+
+所以决定自己写，该插件借鉴了onepage-scroll的一些思想，在此对原作者表示感谢。
+
+
+## 怎么用
+
+直接看demo就好了。
+
+
+## API
+	
+待编辑
+
 
 ## TODO
 
-1. FF mousewheel bug.
-2. Resize,When the section height is lower than the setting min-height,then switch the page to normal scroll,rathor than automatic full page scroll.
-3. menu.
+1. FF mousewheel bug fixed.
+2. Resize,处理resize的情况
+3. 是否需要处理menu.
+4. body动态添加class  
+   比如第一屏添加class fs-page-0 
+   第二屏添加fs-page-1
 
--- jquery.fullscroll.js
+++ jquery.fullscroll.js
@@ -1,283 +1,271 @@
-(function($){
+(function($) {
+
+  $.fn.fullscroll = function(options) {
+
+    var defaults = {
+      sectionSelector: '.section',
+      sectionMinHeight: '480', // TODO
+      middle: true, // middle the content in sections  
+      loop: false, // 
+      animationDuration: 1000, // suggest bigger than 1000ms
+      animationCD: 400, // CD  
+      easing: 'swing', // jquery.easings.min.js
+      easingcss3: 'ease', // 	
+      keyboard: true, // bind keyboard event
+      menu: false, // TODO
+      pagination: true, // circle navi
+      beforeMove: null,
+      afterMove: null,
+      normalSelector: '' //	elem without fire the mousewheel event
+    }
+
+    var settings = $.extend(defaults, options);
+
+    var isScrolling = false,
+    isResizing = false,
+    sectionLength = $(settings.sectionSelector).length,
+    elem = $(this);
+
+    elem.addClass("fs-page");
+
+    $(settings.sectionSelector).addClass("fs-section").wrapInner("<div class='fs-content'></div>").eq(0).addClass("active");
+
+    // middle the content use negative margin-top
+    if (settings.middle) {
+      $(".fs-content").each(function() {
+        var marginTop = -1 * $(this).height() / 2 + "px";
+        $(this).css({
+          "marginTop": marginTop
+        })
+      });
+    }
+
+    $.support.css3translate3d = supportCSS3Translate3d();
+
+    if (!$.support.css3translate3d) {
+      elem.addClass("fs-page-absolute")
+    }
+
+    // circle nav
+    if (settings.pagination) {
+
+      initPagination();
+
+      $(".fs-pagination li a").on("click",
+      function(e) {
+        var index = $(this).parent("li").index();
+        elem.moveTo(index)
+      })
+    }
+
+    function initPagination() {
+
+      $("body").append('<div class="fs-pagination"></div>');
+      var pagination = $(".fs-pagination");
 
+      var html = "<ul>";
+      // TODO add extral class name
+      for (var i = 0; i < sectionLength; i++) {
+        html += '<li><a href="javascript:;"></a></li>';
+      }
+      html += "</ul>";
 
-$.fn.fullscroll = function(options){
+      pagination.append(html).find("li").eq(0).addClass("active");
+      pagination.css("marginTop", pagination.height() / 2 * -1)
 
-	var defaults = {
-		sectionSelector: '.section', 
-		middle:true,  // middle the content in sections  
-		loop: false,   // 
-		animationDuration: 1000,  // suggest bigger than 1000ms
-		animationCD:400, // CD  
-		easing:'swing',   // jquery.easings.min.js
-		easingcss3: 'ease',  // 	
-		keyboard : true,   // bind keyboard event
-		menu:false,   // TODO
-		pagination:true,   // circle navi
-		beforeMove:null, 
-		afterMove:null,
-		normalSelector:'' //	elem without fire the mousewheel event
-	}
-
-	var settings = $.extend(defaults,options);
-	
-	var isScrolling = false,
-		  isResizing = false,
-		  sectionLength = $(settings.sectionSelector).length,
-		  elem =$(this);	
-
-	elem.addClass("fs-page");
-
-	$(settings.sectionSelector).addClass("fs-section").wrapInner("<div class='fs-content'></div>").eq(0).addClass("active");
-
-	// middle the content use negative margin-top
-	if(settings.middle){	
-		$(".fs-content").each(function(){
-			var marginTop = -1*$(this).height()/2+"px";
-			$(this).css({"marginTop":marginTop})
-		});
-	}
-
-	$.support.css3translate3d = supportCSS3Translate3d();
-
-	if(!$.support.css3translate3d){
-		elem.addClass("fs-page-absolute")
-	}
+    }
 
-	// circle nav
-	if(settings.pagination){
-		
-		initPagination();
-
-		$(".fs-pagination li a").on("click",function(e){
-			var index = $(this).parent("li").index();
-			elem.moveTo(index)
-		})
-	}
-
-
-	function initPagination(){
-
-		$("body").append('<div class="fs-pagination"></div>');
-		var pagination = $(".fs-pagination");
+    $.fn.moveTo = function(to, from) {
+
+      if (to == from) {
+        return false;
+      }
 
-		var html = "<ul>";
-		// TODO add extral class name
-		for(var i=0;i<sectionLength;i++){
-			html += '<li><a href="javascript:;"></a></li>';
-		}
-		html += "</ul>";
-
-		pagination.append(html).find("li").eq(0).addClass("active");
-		pagination.css("marginTop",pagination.height()/2*-1)
-
-	}
-
-
-
-	$.fn.moveTo = function(to,from){
-
-		if(to == from){ return false;}
-		
-		if(from==undefined){
-			from = $(".fs-section.active").index();
-		}
-
-		// before move callback
-		if(typeof settings.beforeMove  === "function") {
-			settings.beforeMove(to,from);
-		}
+      if (from == undefined) {
+        from = $(".fs-section.active").index();
+      }
 
-		if(settings.pagination){
-			$(".fs-pagination li").removeClass("active").eq(to).addClass("active");
-		}
+      // before move callback
+      if (typeof settings.beforeMove === "function") {
+        settings.beforeMove(to, from);
+      }
 
-		if(settings.menu){
+      if (settings.pagination) {
+        $(".fs-pagination li").removeClass("active").eq(to).addClass("active");
+      }
 
-		}
+      if (settings.menu) {
+        // TODO
+      }
 
-		$(".fs-section").removeClass("active").eq(to).addClass("active");
+      $(".fs-section").removeClass("active").eq(to).addClass("active");
 
-		var dest = -to*100+"%"
-
-		if($.support.css3translate3d){
-			
-			// use css3 translate3D which can use hardware acceleration
-			elem.css({
-				"-webkit-tranform":"translate3d(0,"+dest+",0)",
-				"-webkit-transition":"all " + settings.animationDuration + "ms " + settings.easingcss3,
-				"-o-tranform":"translate3d(0,"+dest+",0)",
-				"-o-transition":"all " + settings.animationDuration + "ms " + settings.easingcss3,
-				"-moz-transform-tranform":"translate3d(0,"+dest+",0)",
-				"-moz-transition":"all " + settings.animationDuration + "ms " + settings.easingcss3,
-				"-ms-tranform":"translate3d(0,"+dest+",0)",
-				"-ms-transition":"all " + settings.animationDuration + "ms " + settings.easingcss3,
-				"transform":"translate3d(0,"+dest+",0)",
-				"transition": "all " + settings.animationDuration + "ms " + settings.easingcss3
-			});
-
-			// very powerful
-			if(typeof settings.afterMove === "function"){
-				elem.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
-      		settings.afterMove(to,from);
-   			});
-			}
-			
-
-		} else {
-			
-			// use jQuery elem.animate function
-			elem.animate({"top":dest},settings.animationDuration,settings.easing,function(){
-				if(typeof settings.afterMove === "function"){
-					settings.afterMove(to,from);
-				}
-			})
-
-		}
-
-
-	}
-
-
-
-	$.fn.moveDown = function(){
-
-		var current =  $(".fs-section.active")
-		var prev = current.prev(".fs-section");
-		
-		if(!prev.length){
-			settings.loop && (prev = $(".fs-section").last());
-		} 
-			
-		if(prev.length){
-			var from = current.index()
-			var to = prev.index();
-			$.fn.moveTo(to,from);
-		}
-
-	}
-
-
-	$.fn.moveUp = function(){
-
-		var current =  $(".fs-section.active")
-
-		var next = current.next(".fs-section");
-		
-		if(!next.length){
-			settings.loop && (next = $(".fs-section").first());
-		} 
-			
-		if(next.length){
-			var from = current.index()
-			var to = next.index();
-			$.fn.moveTo(to,from);
-		}
-
-	}
-
-
-	// TODO 
-	function resize(){
-		var winWidth = $(window).width(),
-			winHeight = $(window).height();
-	};
-
-
-
-	function mouseWheelHandler(e) {
-
-		e.preventDefault();
-
-		if (!isScrolling) {
-
-			
-			var current = $(".fs-section.active").index();
-
-			
-			// TODO 鼠标事件的兼容处理
-    	var delta = e.originalEvent.wheelDelta || -e.originalEvent.detail;
-			
-			if (delta < 0) {
-				if(!settings.loop && current == sectionLength-1){
-			 		return false;
-			 	}
-			 	elem.moveUp();
-			}else if(delta>0 && current){
-				if(!settings.loop && current == 0){
-			 		return false;
-			 	}
-				elem.moveDown()
-			}
-
-			isScrolling = true; 
-			scrollID = setTimeout(function(){isScrolling = false},settings.animationDuration+settings.animationCD)
-
-		}	
-		
-	}
-
-
-
-	// TODO  FF bug fix
-	// TODO 是否绑定Moz。。。事件
-	$(document).on('mousewheel DOMMouseScroll',mouseWheelHandler);
-
-	if(settings.normalSelector){
-		$("body").on('mousewheel DOMMouseScroll',settings.normalSelector,function(e){
-			e.preventDefault();
-			return false;
-		})
-	}
-
-
-	if(settings.keyboard == true) {
-		$(document).keydown(function(e) {
-      var tag = e.target.tagName.toLowerCase();
-			var which = e.which;
-			if(tag !== 'input' && tag !== 'textarea'){
-				if(which == 33 || which ==37 || which==38){ // 33 page up; 37 left; 38 up
-					elem.moveDown();
-				} else if(which == 32|| which ==34 || which==39 || which==40 ){ //32 space; 34 page down; 39 right; 40 down
-					elem.moveUp();
-				} else if(which==36){ // Home
-					elem.moveTo(0)
-				} else if(which==35){ // End
-					elem.moveTo(sectionLength-1);
-				}
-			}
-		});
-	}
-
-
-	function supportCSS3Translate3d() {
-		var el = document.createElement('p'),
-		has3d,
-		transforms = {
-			'webkitTransform':'-webkit-transform',
-			'OTransform':'-o-transform',
-			'msTransform':'-ms-transform',
-			'MozTransform':'-moz-transform',
-			'transform':'transform'
-		};
-
-		// Add it to the body to get the computed style.
-		document.body.insertBefore(el, null);
-
-		for (var t in transforms) {
-			if (el.style[t] !== undefined) {
-				el.style[t] = "translate3d(1px,1px,1px)";
-				has3d = window.getComputedStyle(el).getPropertyValue(transforms[t]);
-			}
-		}
-
-		document.body.removeChild(el);
-		return (has3d !== undefined && has3d.length > 0 && has3d !== "none");
-	}
-
-
-
-
-}
-
-})(window.jQuery)
+      var dest = -to * 100 + "%"
+
+      if ($.support.css3translate3d) {
+
+        // use css3 translate3D which can use hardware acceleration
+        elem.css({
+          "-webkit-tranform": "translate3d(0," + dest + ",0)",
+          "-webkit-transition": "all " + settings.animationDuration + "ms " + settings.easingcss3,
+          "-o-tranform": "translate3d(0," + dest + ",0)",
+          "-o-transition": "all " + settings.animationDuration + "ms " + settings.easingcss3,
+          "-moz-transform-tranform": "translate3d(0," + dest + ",0)",
+          "-moz-transition": "all " + settings.animationDuration + "ms " + settings.easingcss3,
+          "-ms-tranform": "translate3d(0," + dest + ",0)",
+          "-ms-transition": "all " + settings.animationDuration + "ms " + settings.easingcss3,
+          "transform": "translate3d(0," + dest + ",0)",
+          "transition": "all " + settings.animationDuration + "ms " + settings.easingcss3
+        });
+
+        // very powerful
+        if (typeof settings.afterMove === "function") {
+          elem.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+          function() {
+            settings.afterMove(to, from);
+          });
+        }
+
+      } else {
+
+        // use jQuery elem.animate function
+        elem.animate({"top": dest},settings.animationDuration, settings.easing,function() {
+          if (typeof settings.afterMove === "function") {
+            settings.afterMove(to, from);
+          }
+        })
+
+      }
+
+    }
+
+    $.fn.moveDown = function() {
+
+      var current = $(".fs-section.active") var prev = current.prev(".fs-section");
+
+      if (!prev.length) {
+        settings.loop && (prev = $(".fs-section").last());
+      }
+
+      if (prev.length) {
+        var from = current.index() var to = prev.index();
+        $.fn.moveTo(to, from);
+      }
+
+    }
+
+    $.fn.moveUp = function() {
+
+      var current = $(".fs-section.active")
+
+      var next = current.next(".fs-section");
+
+      if (!next.length) {
+        settings.loop && (next = $(".fs-section").first());
+      }
+
+      if (next.length) {
+        var from = current.index() var to = next.index();
+        $.fn.moveTo(to, from);
+      }
+
+    }
+
+    // TODO 
+    function resize() {
+      var winWidth = $(window).width(),
+      winHeight = $(window).height();
+    };
+
+    function mouseWheelHandler(e) {
+
+      e.preventDefault();
+
+      if (!isScrolling) {
+
+        var current = $(".fs-section.active").index();
+
+        // TODO 鼠标事件的兼容处理
+        var delta = e.originalEvent.wheelDelta || -e.originalEvent.detail;
+
+        if (delta < 0) {
+          if (!settings.loop && current == sectionLength - 1) {
+            return false;
+          }
+          elem.moveUp();
+        } else if (delta > 0) {
+          if (!settings.loop && current == 0) {
+            return false;
+          }
+          elem.moveDown()
+
+        }
+
+        isScrolling = true;
+        scrollID = setTimeout(function() {
+          isScrolling = false
+        },
+        settings.animationDuration + settings.animationCD)
+
+      }
+
+    }
+
+    // TODO  FF bug fix
+    // TODO 是否绑定Moz。。。事件
+    $(document).on('mousewheel DOMMouseScroll', mouseWheelHandler);
+
+    if (settings.normalSelector) {
+      $("body").on('mousewheel DOMMouseScroll', settings.normalSelector,
+      function(e) {
+        e.preventDefault();
+        return false;
+      })
+    }
+
+    if (settings.keyboard == true) {
+      $(document).keydown(function(e) {
+        var tag = e.target.tagName.toLowerCase();
+        var which = e.which;
+        if (tag !== 'input' && tag !== 'textarea') {
+          if (which == 33 || which == 37 || which == 38) { // 33 page up; 37 left; 38 up
+            elem.moveDown();
+          } else if (which == 32 || which == 34 || which == 39 || which == 40) { //32 space; 34 page down; 39 right; 40 down
+            elem.moveUp();
+          } else if (which == 36) { // Home
+            elem.moveTo(0)
+          } else if (which == 35) { // End
+            elem.moveTo(sectionLength - 1);
+          }
+        }
+      });
+    }
+
+    function supportCSS3Translate3d() {
+      var el = document.createElement('p'),
+      has3d,
+      transforms = {
+        'webkitTransform': '-webkit-transform',
+        'OTransform': '-o-transform',
+        'msTransform': '-ms-transform',
+        'MozTransform': '-moz-transform',
+        'transform': 'transform'
+      };
+
+      // Add it to the body to get the computed style.
+      document.body.insertBefore(el, null);
+
+      for (var t in transforms) {
+        if (el.style[t] !== undefined) {
+          el.style[t] = "translate3d(1px,1px,1px)";
+          has3d = window.getComputedStyle(el).getPropertyValue(transforms[t]);
+        }
+      }
+
+      document.body.removeChild(el);
+      return (has3d !== undefined && has3d.length > 0 && has3d !== "none");
+    }
+
+  }
+
+})(jQuery)
